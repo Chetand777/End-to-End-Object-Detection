e8aca366d54ca8257aee0445ee0dd818f3dd5d04
Chetand777 <chetandaddikar31@gmail.com>
2024-05-04 11:51:48 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'signlanguage',
+  version = '0.0.0',
+  author = 'Chetand777',
+  author_email = 'chetandaddikar31@gmail.com',
+  packages = find_packages(),
+  install_requires = []
+)
