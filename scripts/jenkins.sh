2661a2b6fad995fdedde70803ae88f0c5dd2b3f2
Chetand777 <chetandaddikar31@gmail.com>
2024-05-12 16:41:19 +0530

    Docker & Jenkins Files Added

---
+++ scripts/jenkins.sh
@@ -0,0 +1,47 @@
+#|bin/bash
+
+sudo apt update
+
+sudo apt install open-jdk-8-jdk -y
+
+https://pkg.jenkins.io/
+https://pkg.jenkins.io/debian-stable/
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+sudo systemctl status jenkins
+
+
+
+## Instaling Docker
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker $USER
+
+sudo usermod -aG docker jenkins
+
+
+newgrp docker
+
+sudo apt install awscli -y
+
+sudo usermod -a -G docker jenkins
+
+
+## AWS Configuration and restarts jenkins
+
+aws configure
+
+
+## Setup elastic IP on AWS
+
+
+
+## For getting admin password for jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
